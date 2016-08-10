adadf2b666223493f0cefd24fd86de090ba2c910
Feldherren <rpaliwoda@googilemail.com>
2016-08-10 21:05:47 +0100

    More useful links in starting comments. getMail() now gets an email with specified uid. Removed extraneous newline following email text in parseEmail(). Added sendEmailWithAttachment(), which sends an email with a specified attachment to a specified recipient address with a supplied subject. Rewrote testing stuff to use this.

--- llamaserver-helper.py
+++ llamaserver-helper.py
@@ -8,7 +8,10 @@
 # https://www.reddit.com/r/learnpython/comments/3u39i0/is_sending_emails_with_gmail_supported_with/
 # https://support.google.com/mail/troubleshooter/1668960?hl=en-GB&rd=1
 # https://yuji.wordpress.com/2011/06/22/python-imaplib-imap-example-with-gmail/
-# http://naelshiab.com/tutorial-send-email-python/ - includes how to send email with attachment
+# http://robertwdempsey.com/python3-email-with-attachments-using-gmail/
+
+# http://www.tutorialspoint.com/python3/python_multithreading.htm - threading
+# http://tkinter.unpythonic.net/wiki/tkFileDialog - file dialogue
 
 # playing by e-mail:
 # game names cannot have spaces - they use underscores; should match game folder name?
@@ -37,18 +40,29 @@
 # waiting for llamaserver next turn
 # game ended
 
+# emails come from turns@llamaserver.com, always?
+
 # may be able to make application handle more than one game simultaneously: dropdown list to select game, show details and controls for game?
 
 # new config file for each game?
+# shouldn't need to do this? Should just save data in a file somewhere
+
+# data to store about games:
+# name - necessary for finding game folder
+# era - 'early', 'mid', 'late' - part of the generated file names, so necessary to know this to get the correct file
+# nation - possibly needed to parse emails (nation name is mentioned in body), and is used in game file names; necessary as your nation may not be the only file present (also get allied nation files in server-based games)
+# turn number - can get this from latest turn-email from llamaserver
+# state - see above
 
 import imaplib
 import smtplib
 import configparser
 import email
-#from email.MIMEMultipart import MIMEMultipart # gets error, 'No module named email.MIMEMultipart'
-#from email.MIMEText import MIMEText
-#from email.MIMEBase import MIMEBase
-#from email import encoders
+import os
+import sys
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -82,12 +96,16 @@ def getTextBlocks(email_message_instance):
 		blocks.append(email_message_instance.get_payload())
 	return blocks
 	
-def getMail():
-	return
+def getEmail(uid):
+	result, data = mail.uid('fetch', uid, '(RFC822)')
+	raw_email = data[0][1]
+	return raw_email
 
 def parseMail(raw_email):
 	email_message = email.message_from_bytes(raw_email)
 	if email_message['To'] is not None:
+		# only check emails from llamaserver: all llamaserver emails seem to come from turns@llamaserver.com
+		#if email.utils.parseaddr(email_message['From'])[1] == 'turns@llamaserver.com': 
 		email_to = email_message['To']
 		email_from = email.utils.parseaddr(email_message['From']) # for parsing "Yuji Tomita" <yuji@grovemade.com>
 		email_subject = email_message['Subject']
@@ -95,7 +113,7 @@ def parseMail(raw_email):
 		print(email_to)
 		print(email_from)
 		print(email_subject)
-		print(email_text)
+		print(email_text.strip())
 		#for textblock in getTextBlocks(email_message):
 		#	print(textblock)
 		print('-----')
@@ -108,39 +126,44 @@ def listPretenders():
 def getPretender():
 	return
 	
-def sendPretender():
-	try:
-		outmail.login(email, password)
-	except:
-		print("Unexpected error:", sys.exc_info()[0])
-		raise
+def sendEmailWithAttachment(attachment_path, subject, recipient_address):
+	# attachment_path: full path
+	# subject: needs to be the game name, for at least pretender
+	# recipient address: pretenders@llamaserver.net for pretenders, turns@llamaserver.net for turns
 	# create email here
-	msg = MIMEMultipart()
-	msg['From'] = address
-	msg['To'] = 'pretenders@llamaserver.net'
-	msg['Subject'] = "SUBJECT OF THE EMAIL" # needs to be game name
+	outer = MIMEMultipart()
+	outer['From'] = address
+	outer['To'] = recipient_address
+	outer['Subject'] = subject # needs to be game name
 	# make sure it gets and attaches the correct .2h file; should be something like mid_machaka.2h - age_nation.2h; pretender file, though
 	# should be in \savedgames\newlords under datadir
-	# send to llamaserver here
-	outmail.quit()
-	return
-	
-def sendTurn():
+	recipient = recipient_address
 	try:
-		outmail.login(email, password)
+		with open(attachment_path, 'rb') as fp:
+			msg = MIMEBase("application", "octet-stream")
+			msg.set_payload(fp.read())
+		encoders.encode_base64(msg)
+		msg.add_header("Content-Disposition", "attachment", filename=os.path.basename(attachment_path))
+		outer.attach(msg)
 	except:
-		print("Unexpected error:", sys.exc_info()[0])
+		print("Unable to open one of the attachments. Error: ", sys.exc_info()[0])
 		raise
-	# create email here; subject needs to be game name
-	msg = MIMEMultipart()
-	msg['From'] = address
-	msg['To'] = 'turns@llamaserver.net'
-	msg['Subject'] = "SUBJECT OF THE EMAIL" # needs to be game name
-	# make sure it gets and attaches the correct .2h file; should be something like mid_machaka.2h - age_nation.2h
-	# should be in \savedgames\game_name under datadir
-	# send to llamaserver here
-	outmail.quit()
-	return
+	
+	composed = outer.as_string()
+	
+	# send to recipient (llamaserver) here
+	try:
+		with smtplib.SMTP('smtp.gmail.com', 587) as s:
+			s.ehlo()
+			s.starttls()
+			s.ehlo()
+			s.login(address, password)
+			s.sendmail(address, recipient, composed)
+			s.close()
+		print("Email sent!")
+	except:
+		print("Unable to send the email. Error: ", sys.exc_info()[0])
+		raise
 
 mail = imaplib.IMAP4_SSL('imap.gmail.com') # possibly allow users to change that in config, for other email sources
 try:
@@ -158,14 +181,16 @@ outmail.starttls()
 # though this is useful if we know the file for the turn is going to be the latest e-mail
 # http://stackoverflow.com/questions/2983647/how-do-you-iterate-through-each-email-in-your-inbox-using-python has an example for iterating through a mailbox
 result, data = mail.uid('search', None, "ALL") 
-latest_email_uid = data[0].split()[-1] # ditch the [-1], get list of mail IDs? Or do result, data already hold everything?
-result, data = mail.uid('fetch', latest_email_uid, '(RFC822)')
-#print(result) # 'OK'
-#for thing in data:
-#	print(thing)
-# (b'1 (UID 5 RFC822 {3332}', text of email 
-# b')'
-raw_email = data[0][1]
-parseMail(raw_email)
+#latest_email_uid = data[0].split()[-1] # ditch the [-1], get list of mail IDs? Or do result, data already hold everything?
+#raw_email = getEmail(latest_email_uid)
+#parseMail(raw_email)
+
+# gets all emails in the current mailbox; the inbox, in this case
+email_uids = data[0].split()
+for uid in email_uids:
+	raw_email = getEmail(uid)
+	parseMail(raw_email)
+
+sendEmailWithAttachment(r"C:\Users\Rebecca\AppData\Roaming\Dominions4\savedgames\newlords\mid_ys_0.2h", "test_game", "rpaliwoda@googlemail.com")
 
 # https://yuji.wordpress.com/2011/06/22/python-imaplib-imap-example-with-gmail/ mentions we need to do other stuff to get at the text of it. Not sure how to get at the attachment, as we may not care about the text
